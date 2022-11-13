1ac68691965243554d13e7668d60b17e6923e641
Ayoub BERDEDDOUCH <ayoub.berd@gmail.com>
2022-11-13 14:58:03 +0100

    Adding the whole steamlit files

---
+++ app5.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import pandas as pd
+
+df = pd.read_excel('Green_Algeria_Project_Contributions.xlsx')
+   
+def app():
+    st.title("🌱 Omdena Algeria Chapter")
+    img=Image.open("Images/omdena_algeria.png")
+    newsize=(280,300)
+    img1=img.resize(newsize)
+    st.write("\n")
+    st.write("\n")
+    st.markdown("""Collaborate, Learn, Build, Grow """)
+    st.subheader("Omdena Algeria Local Chapter for both Part: 1 and Part: 2.")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.image(img1,use_column_width='always')
+    st.write("\n")
+    st.header("🤞Contributors")
+    st.dataframe(df)
+    st.write("\n")
+    st.header("🌐Website")
+    link='check out this [link](https://omdena.com/local-chapters/algeria-local-chapter/)'
+    st.markdown(link,unsafe_allow_html=True)
+    st.markdown("© 2022 Omdena")
